3562c48c6425ab29e934df7300961d070dea34e5
dilnazbaurzhan <dbbaurzhanova@gmail.com>
2023-04-12 15:52:13 +0600

    have finished Lab9

---
+++ lab9/hh_back/api/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Company, Vacancy 
+
+# Register your models here.
+@admin.register(Company) 
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'city', 'address')   #id добавить?
+
+@admin.register(Vacancy) 
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'salary', 'company')
+
